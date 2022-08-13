2dd09a93e88d23a442eb6a74c8d94b892f6965fc
Nemirtingas <nemirtingas@noreply.com>
2022-08-13 11:13:51 +0200

    Add MacOS opengl2 test app.

---
+++ tests/macos_opengl2/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+cmake -DIMGUI_USER_CONFIG="$(pwd)/ingameoverlay_imconfig.h" -DBUILD_INGAMEOVERLAY_TESTS=ON -S ../../ -B ../../OUT/macos_opengl2 &&\
+cmake --build ../../OUT/macos_opengl2
+
+#../../OUT/macos_opengl2/macos_opengl2_app
---
+++ tests/macos_opengl2/ingameoverlay_imconfig.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stdint.h>
+// ImTextureID [configurable type: override in imconfig.h with '#define ImTextureID xxx']
+#define ImTextureID uint64_t
