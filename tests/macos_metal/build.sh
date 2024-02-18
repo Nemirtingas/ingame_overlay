4c78d461272482f264a2bdca063de3260944a02e
Nemirtingas <nemirtingas@noreply.com>
2024-02-18 17:02:55 +0100

    Fix tests

--- tests/macos_metal/build.sh
+++ tests/macos_metal/build.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-cmake -DIMGUI_USER_CONFIG="$(pwd)/ingameoverlay_imconfig.h" -DBUILD_INGAMEOVERLAY_TESTS=ON -S ../../ -B ../../OUT/macos_metal &&\
+cmake -DIMGUI_USER_CONFIG="$(pwd)/ingameoverlay_imconfig.h" -DINGAMEOVERLAY_BUILD_TESTS=ON -S ../../ -B ../../OUT/macos_metal &&\
 cmake --build ../../OUT/macos_metal
 
 #../../OUT/macos_metal/macos_metal_app
