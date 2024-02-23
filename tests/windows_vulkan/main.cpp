7c607d934a1ab41a9f218cf78d6961057eb64071
Nemirtingas <nemirtingas@noreply.com>
2024-02-23 03:02:12 +0100

    Use an untouched backend for the test app

--- tests/windows_vulkan/main.cpp
+++ tests/windows_vulkan/main.cpp
@@ -15,7 +15,7 @@
 
 #include <imgui.h>
 #include <backends/imgui_impl_glfw.h>
-#include <backends/imgui_impl_vulkan.h>
+#include "imgui_impl_vulkan.h"
 #include <stdio.h>          // printf, fprintf
 #include <stdlib.h>         // abort
 #define GLFW_INCLUDE_NONE
