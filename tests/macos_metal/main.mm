930683940b7b6d21e35ec4948acd8515ba6c3d12
Nemirtingas <nemirtingas@noreply.com>
2024-02-24 15:24:33 +0100

    Fix

--- tests/macos_metal/main.mm
+++ tests/macos_metal/main.mm
@@ -17,6 +17,9 @@
 #include <backends/imgui_impl_metal.h>
 #include "imgui_impl_osx.h"
 @interface AppViewController : NSViewController<NSWindowDelegate>
+@end
+
+@interface AppViewController () <MTKViewDelegate>
 @property (nonatomic, readonly) MTKView *mtkView;
 @property (nonatomic, strong) id <MTLDevice> device;
 @property (nonatomic, strong) id <MTLCommandQueue> commandQueue;
